66d04a7ceb785459afd1dd631973423a45b218ec
Bryan Melisse <bryu34@gmail.com>
2024-03-20 20:02:15 +0100

    Réorganisé la page d'Accueil du site de recette de cuisine

--- Recette_de_cuisine/index.php
+++ Recette_de_cuisine/index.php
@@ -1,136 +1,35 @@
 <?php 
 
-// Dans ce fichier nous allons créer une page de recette de cuisine
-
-// On veut 4 recettes venant de 4 utilisateur different.
-
-// $recipes = [
-//     [
-//         'title' => 'Blanquet de veau',
-//         'author' => 'Didier Mamourn', 
-//         'email' => 'didier.mamourn@example.com',
-//     ],
-//     [ 
-//         'title' => 'Grattin de Dauphinois',
-//         'author' => 'Jean Salaur', 
-//         'email' => 'jean.salaur@example.com',
-//     ],
-//     [   
-//         'title' => 'Salade de fruits exotiques',
-//         'author' => 'Kevin Renard', 
-//         'email' => 'kevin.renard@example.com',
-//     ],
-//     [
-//         'title' => 'Lasagnes aux épinards',
-//         'author' => 'Salomon Gregoire', 
-//         'email' => 'salomon.gregoire@example.com',
-//     ],
-// ];
-
-$recipes = [
-    [
-        'title' => 'Cassoulet',
-        'recipe' => 'Etape 1 : des flageolets !',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Couscous',
-        'recipe' => 'Etape 1 : de la semoule',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => false,
-    ],
-    [
-        'title' => 'Escalope milanaise',
-        'recipe' => 'Etape 1 : prenez une belle escalope',
-        'author' => 'mathieu.nebra@exemple.com',
-        'is_enabled' => true,
-    ],
-];
-
-$users = [
-    [
-        'full_name' => 'Mickaël Andrieu',
-        'email' => 'mickael.andrieu@exemple.com',
-        'age' => 34,
-    ],
-    [
-        'full_name' => 'Mathieu Nebra',
-        'email' => 'mathieu.nebra@exemple.com',
-        'age' => 34,
-    ],
-    [
-        'full_name' => 'Laurène Castor',
-        'email' => 'laurene.castor@exemple.com',
-        'age' => 28,
-    ],
-];
-
-
-// Maintenant on va créer des fonctions pour nous faciliter la tache les prochaines fois 
-
-function isValidRecipe(array $recipe) : bool
-{
-    if (array_key_exists('is_enabled', $recipe)) {
-        $isEnabled = $recipe['is_enabled'];
-    } else {
-        $isEnabled = false;
-    }
-
-    return $isEnabled;
-}
-
-// Deuxième fonction : getRecipes
-
-function getRecipes(array $recipes) : array
-{
-    $validRecipes = [];
-
-    foreach ($recipes as $recipe) {
-        if (isValidRecipe($recipe)) {
-            $validRecipes[] = $recipe;
-        }
-    }
-
-    return $validRecipes;
-}
-
-// Troisième fonction : displayAuthor
-
-function displayAuthor(string $authorEmail, array $users) : string 
-{
-    foreach ($users as $user) {
-        if ($authorEmail === $user['email']) {
-            return $user['full_name']. '(' .$user['age'].' ans)';
-        }
-    }
-}
-
+require_once(__DIR__ .'/variables.php');
+require_once(__DIR__ .'/functions.php');
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Recettes de cuisine</title>
-    <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-    >
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de recettes - Page d'accueil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <div class="container">
-    <h1>Liste des recettes de cuisine</h1>
-    <?php
-    $valid_recipes = getRecipes($recipes);
-    
-    foreach ($valid_recipes as $recipe) {
-        echo '<h2>'.$recipe['title'].'</h2>';
-        echo $recipe['recipe'].'<br/>';
-        echo '<i>'.displayAuthor($recipe['author'], $users).'</i>';
-    }
 
-    ?>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+        <!-- inclusion de l'entête du site -->
+        <?php require_once(__DIR__ . '/header.php'); ?>
+        <h1>Site de recettes</h1>
+        <?php foreach (getRecipes($recipes) as $recipe) : ?>
+            <article>
+                <h3><?php echo $recipe['title']; ?></h3>
+                <div><?php echo $recipe['recipe']; ?></div>
+                <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+            </article>
+        <?php endforeach ?>
     </div>
+    <!-- inclusion du bas de page du site -->
+    <?php require_once(__DIR__ . '/footer.php'); ?>
 </body>
+
 </html>
