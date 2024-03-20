25196e87dd1c4eb6f7a3468c0b326e29dee121c0
Bryan Melisse <bryu34@gmail.com>
2024-03-20 17:13:45 +0100

    Ajouter les fonctions dans le fichier index du projet fil rouge

--- Recette_de_cuisine/index.php
+++ Recette_de_cuisine/index.php
@@ -48,6 +48,64 @@ $recipes = [
     ],
 ];
 
+$users = [
+    [
+        'full_name' => 'Mickaël Andrieu',
+        'email' => 'mickael.andrieu@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Mathieu Nebra',
+        'email' => 'mathieu.nebra@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Laurène Castor',
+        'email' => 'laurene.castor@exemple.com',
+        'age' => 28,
+    ],
+];
+
+
+// Maintenant on va créer des fonctions pour nous faciliter la tache les prochaines fois 
+
+function isValidRecipe(array $recipe) : bool
+{
+    if (array_key_exists('is_enabled', $recipe)) {
+        $isEnabled = $recipe['is_enabled'];
+    } else {
+        $isEnabled = false;
+    }
+
+    return $isEnabled;
+}
+
+// Deuxième fonction : getRecipes
+
+function getRecipes(array $recipes) : array
+{
+    $validRecipes = [];
+
+    foreach ($recipes as $recipe) {
+        if (isValidRecipe($recipe)) {
+            $validRecipes[] = $recipe;
+        }
+    }
+
+    return $validRecipes;
+}
+
+// Troisième fonction : displayAuthor
+
+function displayAuthor(string $authorEmail, array $users) : string 
+{
+    foreach ($users as $user) {
+        if ($authorEmail === $user['email']) {
+            return $user['full_name']. '(' .$user['age'].' ans)';
+        }
+    }
+}
+
 
 ?>
 
