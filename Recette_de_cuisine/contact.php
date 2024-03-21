ba31e87adbd4a10d83bad4c71afb67e3b75a8d8c
Bryan Melisse <bryu34@gmail.com>
2024-03-21 16:47:29 +0100

    Changé les fichiers contact afin de prendre en compte l'importation des fichiers

--- Recette_de_cuisine/contact.php
+++ Recette_de_cuisine/contact.php
@@ -15,11 +15,13 @@
     <div class="container">
         <?php require_once(__DIR__ . '/header.php'); ?>
         <h1>Contactez nous</h1>
-        <form action="submit_contact.php" method="POST">
+        <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
                 <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
+                <label for="screenshot" class="form-label">Votre capture d'écran</label>
+                <input type="file" class="form-control" id="screenshot" name="screenshot" />
             </div>
             <div class="mb-3">
                 <label for="message" class="form-label">Votre message</label>
